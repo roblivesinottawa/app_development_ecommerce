d6843d0fade27594331f49cfb332c09c38b4e17e
Robson Ferreira <tech.rob@icloud.com>
2022-03-16 13:37:14 -0400

    initial commit

---
+++ sql_scripts/load_ecommerce_tables.sql
@@ -0,0 +1,4 @@
+COPY product_orders FROM '/Users/rob_the_programmer/Documents/programming/2022/sql_app_development/data/product_orders.csv' DELIMITER ',' CSV HEADER;
+COPY products FROM '/Users/rob_the_programmer/Documents/programming/2022/sql_app_development/data/products.csv' DELIMITER ',' CSV HEADER;
+COPY orders FROM '/Users/rob_the_programmer/Documents/programming/2022/sql_app_development/data/orders.csv' DELIMITER ',' CSV HEADER;
+COPY customers FROM '/Users/rob_the_programmer/Documents/programming/2022/sql_app_development/data/customers.csv' DELIMITER ',' CSV HEADER;
